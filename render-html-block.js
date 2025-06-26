d29c1a264c6b17d0917af379b10ab220cf396028
carmelslam <carmelglobalmarketing@gmail.com>
2025-06-26 23:11:44 +0300

    Sanitize HTML before injecting

--- render-html-block.js
+++ render-html-block.js
@@ -13,3 +13,11 @@ export function renderHTMLBlock(html, map) {
     return val !== undefined ? escapeHTML(val) : match;
   });
 }
+
+// Basic sanitization to strip script tags and event attributes
+export function sanitizeHTML(html) {
+  return String(html)
+    .replace(/<script[^>]*>.*?<\/script>/gis, '')
+    .replace(/\son\w+="[^"]*"/gi, '')
+    .replace(/\son\w+='[^']*'/gi, '');
+}
