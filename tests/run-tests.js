5cd94eccb6cedbef0eeab5d310b9d90bbcc59c76
carmelslam <carmelglobalmarketing@gmail.com>
2025-07-04 19:42:06 +0300

    Fix login redirect and test environment

--- tests/run-tests.js
+++ tests/run-tests.js
@@ -1,4 +1,24 @@
-import './renderHTMLBlock.test.js';
-import './validation.test.js';
-import './feeCalculation.test.js';
-import './finalReportDepreciation.test.js';
+// Provide a minimal sessionStorage mock for tests that rely on browser APIs
+global.sessionStorage = {
+  _store: {},
+  getItem(key) {
+    return Object.prototype.hasOwnProperty.call(this._store, key)
+      ? this._store[key]
+      : null;
+  },
+  setItem(key, value) {
+    this._store[key] = String(value);
+  },
+  removeItem(key) {
+    delete this._store[key];
+  },
+  clear() {
+    this._store = {};
+  }
+};
+
+// Dynamically load the test files after the environment is prepared
+await import('./renderHTMLBlock.test.js');
+await import('./validation.test.js');
+await import('./feeCalculation.test.js');
+await import('./finalReportDepreciation.test.js');
