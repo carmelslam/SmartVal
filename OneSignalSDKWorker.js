71bede0780d7e88c9028114a61271df5800d398c
carmelslam <carmelglobalmarketing@gmail.com>
2025-07-18 21:09:46 +0300

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -4,3 +4,6 @@ importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 self.addEventListener('message', function(event) {
   // OneSignal SDK will handle the message
 });
+
+// Version stamp to force service worker update
+const SW_VERSION = "1.0.1";
