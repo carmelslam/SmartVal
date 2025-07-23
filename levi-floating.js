e58fc6da3741550096d345533f41112115e7c70b
carmelslam <carmelglobalmarketing@gmail.com>
2025-07-23 13:43:24 +0300

    Update levi-floating.js

--- levi-floating.js
+++ levi-floating.js
@@ -368,7 +368,6 @@
 
   // Global functions
   window.toggleLeviReport = function () {
-    console.log('👆 toggleLeviReport clicked - function is working');
     const modal = document.getElementById("leviModal");
     if (modal.style.display === "none" || !modal.style.display) {
       loadLeviData();
