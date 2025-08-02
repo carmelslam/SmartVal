7fb6fc37325ea308cac239773040dc98ddb31773
carmelslam <carmelglobalmarketing@gmail.com>
2025-08-02 15:52:42 +0200

    Update helper.js

--- helper.js
+++ helper.js
@@ -2470,8 +2470,9 @@ function processDirectData(data, result) {
     'שווי מצטבר מספר בעלים': ['valuation.adjustments.ownership_history.cumulative'],
     
     // Features adjustments
-    'מאפיינים': ['valuation.adjustments.features.description'],
-    'ערך מאפיינים': ['valuation.adjustments.features.value', 'valuation.adjustments.features.תיאור מאפיינים'],
+    'מאפיינים': ['valuation.adjustments.features.תיאור מאפיינים'],
+    'ערך מאפיינים': ['valuation.adjustments.features.value'],
+    'תיאור מאפיינים': ['valuation.adjustments.features.תיאור מאפיינים'],
     'מחיר מאפיינים %': ['valuation.adjustments.features.percent'],
     'ערך ש"ח מאפיינים': ['valuation.adjustments.features.amount'],
     'שווי מצטבר מאפיינים': ['valuation.adjustments.features.cumulative'],
